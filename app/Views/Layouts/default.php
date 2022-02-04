dd417b9846ede60f8ed3668fd0f6876bddb0b0a0
NiculaGabriel <nicula.gabriel87@yahoo.com>
2022-02-04 16:07:22 +0200

    remove home from menu

--- app/Views/Layouts/default.php
+++ app/Views/Layouts/default.php
@@ -17,10 +17,7 @@
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="<?= base_url('/')?>">Home <span class="sr-only">(current)</span></a>
-          </li>
+        <ul class="navbar-nav mr-auto">           
           <li class="nav-item">
             <a class="nav-link" href="<?= base_url('/contracte')?>">Contracte</a>
           </li>
